40b821bd4035a996cb8feb12754a9fca2ccdfe6d
Mike Meyer <mikemey01@gmail.com>
2013-09-22 22:22:41 -0600

    removed DeviceListActivity errors

--- src/com/chariotinstruments/chariotgauge/DeviceListActivity.java
+++ src/com/chariotinstruments/chariotgauge/DeviceListActivity.java
@@ -194,13 +194,7 @@ public class DeviceListActivity extends Activity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // If it's already paired, skip it, because it's been listed already
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                	if(device.getName().equalsIgnoreCase("linvor") ||
-                		device.getName().equalsIgnoreCase("HC-06") ||
-                		device.getName().equalsIgnoreCase("null")){
-                		mNewDevicesArrayAdapter.add("Chariot Gauge" + "\n" + device.getAddress());
-                	}else{
-                		mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                	}
+                	mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 }
             // When discovery is finished, change the Activity title
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
