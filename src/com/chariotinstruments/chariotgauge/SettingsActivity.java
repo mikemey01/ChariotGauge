d9c7838a2e738473c0d8a58497f584b948bd345a
Mike Meyer <mikemey01@gmail.com>
2013-10-16 22:13:27 -0600

    added link to site in prefs

--- src/com/chariotinstruments/chariotgauge/SettingsActivity.java
+++ src/com/chariotinstruments/chariotgauge/SettingsActivity.java
@@ -1,7 +1,13 @@
 package com.chariotinstruments.chariotgauge;
 
 
+import java.util.prefs.Preferences;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.view.View;
@@ -12,8 +18,18 @@ public class SettingsActivity extends PreferenceActivity {
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.prefs);
-          
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+        getPreferenceManager()
+        .findPreference("go_to_site")
+        .setOnPreferenceClickListener(
+           new OnPreferenceClickListener() {
+        	 public boolean onPreferenceClick(Preference preference) {
+             Intent intent = new Intent(Intent.ACTION_VIEW);
+             intent.setData(Uri.parse("https://www.chariotgauge.com/product/controller/"));
+             startActivity(intent);
+             return true;
+         }
+     });
 	}
 }
