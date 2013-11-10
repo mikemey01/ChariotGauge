543237799e90d8a849aaab37e0903122c840c17a
Mike Meyer <mikemey01@gmail.com>
2013-11-10 10:02:02 -0700

    cleanup

--- src/com/chariotinstruments/chariotgauge/PSensor.java
+++ src/com/chariotinstruments/chariotgauge/PSensor.java
@@ -30,7 +30,7 @@ public class PSensor extends Activity {
 	private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT      = 2;
     
- // Debugging
+    // Debugging
     private static final String TAG = "ProjectSensor";
     private static final boolean D  = true;
 
