77dc58931888dd996c0b6da112ff74c497757690
mikemey01 <mikemey01@gmail.com>
2014-05-19 21:32:44 -0600

    testing 1000 points at 50ms

--- charting/com/chariotinstruments/chariotgauge/SingleChartActivity.java
+++ charting/com/chariotinstruments/chariotgauge/SingleChartActivity.java
@@ -29,7 +29,7 @@ public class SingleChartActivity extends Activity {
         thread = new Thread() {
             public void run()
             {
-                for (int i = 0; i < 100; i++) 
+                for (int i = 0; i < 1000; i++) 
                 {
                     try {
                         Thread.sleep(50);
