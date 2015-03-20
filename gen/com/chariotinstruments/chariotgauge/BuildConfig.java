524a689257d448e16b501d98ef6f39b5b7b1fd0a
highvelcty <ericallenmeyer@gmail.com>
2015-03-19 20:43:22 -0600

    Transition to Android Studio
    Transition from Eclipse to Android Studio. The project folder structure
    is very different with this change.

--- gen/com/chariotinstruments/chariotgauge/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.chariotinstruments.chariotgauge;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
